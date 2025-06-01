19407228726e34915e69f798837f78121dc50531
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2025-06-01 16:52:51 +0300

    refactor: small changes on admins page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -13,7 +13,7 @@
                 await invoke('tauri_get_session');
             userName = session.sub;
         } catch {
-            goto('/login');
+            goto('/');
         } finally {
             loading = false;
         }
@@ -33,7 +33,7 @@
             await invoke('tauri_logout');
             goto('/');
         } catch (err) {
-            logoutError = typeof err === 'string' ? err : err.toString();
+            logoutError = String(err);
         }
     }
 </script>
@@ -43,11 +43,7 @@
         <span class="loading loading-spinner text-primary"></span>
     </div>
 {:else}
-    <div class="p-6 space-y-6 container">
-        <h2 class="text-3xl font-semibold">
-            Logged as: <span class="text-primary">{userName}</span>
-        </h2>
-
+    <div class="p-6 w-full">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div
                     class="card bg-base-300 shadow hover:bg-neutral cursor-pointer transition"
@@ -68,15 +64,7 @@
                     <p>Update your password</p>
                 </div>
             </div>
-            <div
-                    class="card bg-base-300 shadow hover:bg-neutral cursor-pointer transition"
-                    on:click={handleLogout}
-            >
-                <div class="card-body text-left">
-                    <h3 class="card-title">Log out</h3>
-                    <p>Return to the login screen</p>
-                </div>
-            </div>
+
         </div>
     </div>
 {/if}
