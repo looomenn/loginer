e007c317e13a4080d27db2af9f5b608e3e04d059
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2025-06-01 16:51:22 +0300

    feat: add redirect logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,35 @@
 <script>
-    import Footer from "$lib/components/Footer.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
     import "../app.css";
+
+    import { onMount } from 'svelte';
+    import { invoke } from "@tauri-apps/api/core";
+    import { goto } from '$app/navigation';
+    import { page } from "$app/state";
+
+    let name = "";
+    let role = "";
+
+    onMount(async () => {
+        try {
+            const session = await invoke("tauri_get_session");
+            name = session.sub;
+            role = session.role;
+
+            if (page.url.pathname === "/") {
+                goto(role === "admin" ? "/admin" : "/user");
+            }
+        } catch {
+            if (page.url.pathname !== "/") {
+                goto("/");
+            }
+        }
+
+    });
 </script>
 
-<slot></slot>
+{#if page.url.pathname !== "/"}
+    <Navbar />
+{/if}
+
+<slot/>
