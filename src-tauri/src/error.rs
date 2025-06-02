42e4ac64dd8cd649928f7cdb0b81a6bbe9739174
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2025-06-02 06:52:13 +0300

    feat: add rsa errors impl

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -32,12 +32,63 @@ pub enum AppError {
     #[error("Other error: {0}")]
     Other(String),
 
+    #[error("Error: {0}")]
+    Error(String),
 
     #[error("Internal error: {0}")]
     Internal(String),
+
+    #[error("IO error: {0}")]
+    Io(String),
 }
 
 pub type Result<T> = std::result::Result<T, AppError>;
+
+
+impl From<std::io::Error> for AppError {
+    fn from(e: std::io::Error) -> Self {
+        AppError::Io(e.to_string())
+    }
+}
+
+impl From<rsa::errors::Error> for AppError {
+    fn from(e: rsa::errors::Error) -> Self {
+        AppError::Internal(format!("RSA error: {}", e))
+    }
+}
+
+impl From<String> for AppError {
+    fn from(e: String) -> Self {
+        AppError::Other(e)
+    }
+}
+
+impl From<AppError> for InvokeError {
+    fn from(e: AppError) -> Self {
+        InvokeError::from(e.to_string())
+    }
+}
+
+
+impl From<rsa::pkcs1::Error> for AppError {
+    fn from(e: rsa::pkcs1::Error) -> Self {
+        AppError::Internal(format!("RSA PKCS1 error: {}", e))
+    }
+}
+
+impl From<rsa::pkcs8::spki::Error> for AppError {
+    fn from(e: rsa::pkcs8::spki::Error) -> Self {
+        AppError::Internal(format!("RSA PKCS8 error: {}", e))
+    }
+}
+
+impl From<rsa::pkcs8::Error> for AppError {
+    fn from(e: rsa::pkcs8::Error) -> Self {
+        AppError::Internal(format!("RSA PKCS8 error: {}", e))
+    }
+}
+
+
 
 impl From<argon2::Error> for AppError {
     fn from(e: argon2::Error) -> Self {
@@ -47,11 +98,5 @@ impl From<argon2::Error> for AppError {
 impl From<argon2::password_hash::Error> for AppError {
     fn from(e: argon2::password_hash::Error) -> Self {
         AppError::HashError(e.to_string())
-    }
-}
-
-impl From<AppError> for InvokeError {
-    fn from(e: AppError) -> Self {
-        InvokeError::from(e.to_string())
     }
 }
